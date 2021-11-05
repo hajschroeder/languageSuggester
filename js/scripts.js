ba73e1248a8dad89f0734bb6c1b3517a857a985e
Hans Schroeder <hajschroeder@gmail.com>
2021-11-05 13:57:16 -0700

    included more html and CSS stylings

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
+function gender(option){
+  return option;
+}
+
 $(document).ready(function(){
+  console.log("Script executing!");
   $("#basics").submit(function(event){
+    console.log("Submit form reached");
     event.preventDefault();
     const nameInput= $("input#name").val();
     const ageInput= $("input#age").val();
